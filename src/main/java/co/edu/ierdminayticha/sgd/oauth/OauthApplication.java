0ec1be7ea0ceb75a842aacd0866ecc21c3621c0b
Pedro Luis <plchavez@ucundinamarca.edu.co>
2021-07-29 17:31:36 -0500

    Refactor

--- src/main/java/co/edu/ierdminayticha/sgd/oauth/OauthApplication.java
+++ src/main/java/co/edu/ierdminayticha/sgd/oauth/OauthApplication.java
@@ -1,21 +1,15 @@
 package co.edu.ierdminayticha.sgd.oauth;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class OauthApplication implements CommandLineRunner {
-
-	@Autowired
-	private BCryptPasswordEncoder passwordEncoder;
+public class OauthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthApplication.class, args);
@@ -25,17 +19,6 @@ public class OauthApplication implements CommandLineRunner {
 	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		String password = "1234";
-
-		for (int i = 0; i < 4; i++) {
-			String passwordBCryp = passwordEncoder.encode(password);
-			System.out.println(passwordBCryp);
-		}
-
 	}
 
 }
--- src/main/java/co/edu/ierdminayticha/sgd/oauth/service/UserServiceImpl.java
+++ src/main/java/co/edu/ierdminayticha/sgd/oauth/service/UserServiceImpl.java
@@ -80,7 +80,5 @@ public class UserServiceImpl implements IUserService, UserDetailsService {
 		}
 		return response;
 	}
-	
-	
 
 }
